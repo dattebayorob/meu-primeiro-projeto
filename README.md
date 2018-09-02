542af1b31052c6448ffcb98f08655f1bb01405cd
dattebayoRob <robson.william65@gmail.com>
2018-09-02 11:21:22 -0300

    inicio

---
+++ README.md
@@ -0,0 +1,3 @@
+[![Build Status](https://travis-ci.org/dattebayorob/meu-primeiro-projeto.svg?branch=master)](https://travis-ci.org/dattebayorob/meu-primeiro-projeto)
+# meu-primeiro-projeto
+Projeto de testes criados para estudos de spring boot
---
+++ src/main/java/com/dtb/api/MeuPrimeiroProjetoApplication.java
@@ -0,0 +1,13 @@
+package com.dtb.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuPrimeiroProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
+		System.out.println("Apenas um teste besta");
+	}
+}
